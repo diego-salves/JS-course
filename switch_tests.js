6b69ada600bfa09e8499e044294f9c381f806d63
Diego Santos <diegosantos57@outlook.com.br>
2022-02-22 22:08:01 -0300

    update switch case

---
+++ switch_tests.js
@@ -0,0 +1,13 @@
+var x = 1;
+var y = 3;
+
+switch (y) { 
+    case 2:
+        console.log("oh yeah")
+        break;
+    case 1:
+        console.log(`oh no ${x} is good`)
+        break;
+    default:
+        console.log(`Not today mate ${x}, ${y}`)
+}
