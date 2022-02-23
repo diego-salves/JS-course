8a9fca6d5973a8c32807148ef76eef0209b6a205
Diego Santos <diegosantos57@outlook.com.br>
2022-02-22 22:56:32 -0300

    Create operador_ternario.js

---
+++ operador_ternario.js
@@ -0,0 +1,11 @@
+var sexo = "m"
+
+if (sexo === "m"){
+    console.log("Masculino")
+} else {
+    console.log("Feminino")
+};
+
+console.log(sexo === "m" ? "Masculino" : "Feminino")
+
+//apenas if else simples
