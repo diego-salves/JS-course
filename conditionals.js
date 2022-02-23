6a7c8a567c4f74eab372bbbe7553439ddf001b3b
Diego Santos <diegosantos57@outlook.com.br>
2022-02-22 22:33:02 -0300

    Create conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+for (var i = 0; i < 10 ; i++){
+    console.log(i);
+}
+
+var numero = 2;
+// declarar sempre antes a variavel
+while (numero < 10) {
+    console.log(numero);
+    numero++;
+}
+
+//do-while, primeiro executa depois checa
+var num = 0
+
+do {
+    console.log(num);
+    num++;
+} while ( num < 10);
