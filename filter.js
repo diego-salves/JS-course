96305db4efc2acf08882cf49362bfb47625667ee
Diego Santos <diegosantos57@outlook.com.br>
2022-02-23 22:58:54 -0300

    reduce applied

---
+++ filter.js
@@ -0,0 +1,5 @@
+//O código a seguir mapeia um array de números e cria um novo array contendo o valor da raiz quadrada de cada número do primeiro array.
+
+var numbers = [1, 4, 9];
+var roots = numbers.map(Math.sqrt);
+// roots é [1, 2, 3], numbers ainda é [1, 4, 9]
