01a00c21926ff9a18ca4aa9721a9fa8acbaaf57f
Diego Santos <diegosantos57@outlook.com.br>
2022-02-23 22:50:23 -0300

    map, array

---
+++ arrays.js
@@ -0,0 +1,8 @@
+shift
+Pop
+splice
+push
+unshift
+slice
+concat
+//arrays dentro de arrays
---
+++ for-each.js
@@ -0,0 +1,8 @@
+function logArrayElements(element, index, array) {
+    console.log("a[" + index + "] = " + element);
+}
+[2, 5, 9].forEach(logArrayElements);
+// logs:
+// a[0] = 2
+// a[1] = 5
+// a[2] = 9
