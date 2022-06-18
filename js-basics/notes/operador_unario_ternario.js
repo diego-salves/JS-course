19b60a298ba498626db0f4264f9e545aee97ed80
Diego Santos <diegosantos57@outlook.com.br>
2022-06-18 00:40:49 -0300

    starting functions

---
+++ js-basics/notes/operador_unario_ternario.js
@@ -0,0 +1,14 @@
+//operador unario e ternario
+
+//unario = operação com um elemento apenas
+
+var idade = 25;
+idade++; //operação unária
+
+console.log(idade);
+
+//ternario = condição ? true : false;
+
+var sexo = 'm';
+
+console.log(sexo === 'm' ? "Masculino" : "Feminino");
