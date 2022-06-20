426ba24480b7fd39f37583e13e4b7cd15aa5a3e6
Diego Santos <diegosantos57@outlook.com.br>
2022-06-19 22:55:09 -0300

    created a simple bootstrap project to try some features

--- boostrap-website/dist/js/scripts.js
+++ boostrap-website/dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Heroic Features v5.0.5 (https://startbootstrap.com/template/heroic-features)
+* Start Bootstrap - JS basics v1.0.0 (localhost:3000)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/JS-basics/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
