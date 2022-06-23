f1031d6b9f03c01d44851c7c13e9962310ad6132
Diego Santos <diegosantos57@outlook.com.br>
2022-06-23 01:08:07 -0300

    updated map content

--- js-basics/notes/foreach-map-filter-reduce.js
+++ js-basics/notes/foreach-map-filter-reduce.js
@@ -25,3 +25,35 @@ cursos.forEach(print); //para cada elemento dentro do array cursos, execute a fu
 cursos.forEach(function(curso, indice){
     console.log(`${indice} - ${curso}`);         //template string, acessa variaveis através do $ e chave {}
 });
+
+
+//MAP
+
+//mapeia os dados do array, transforma, gera um novo array de mesmo tamanho com dados alterados
+
+var values = [2, 4, 6, 8, 10];
+
+var dobro = values.map(function(valor){
+    return valor * 2;
+});
+
+function dobrar(valor){
+    return valor * 2
+};
+
+//console.log(dobro);
+
+//encadeando maps
+
+function somar_4(valor){
+    return valor + 4;
+}
+
+function dividir_por_5(valor){
+    return valor / 5;
+}
+
+var result = values.map(dobrar).map(somar_4).map(dividir_por_5);
+console.log(result);
+
+//diferença do foreach é que o map retorna um novo array
