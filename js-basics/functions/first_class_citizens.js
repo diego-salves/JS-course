ad63f9d8f8ddb841e8efe0ab6179f63d042b2d32
diego-salves <diegosantos57@outlook.com.br>
2022-06-29 22:42:00 -0300

    starting functions

---
+++ js-basics/functions/first_class_citizens.js
@@ -0,0 +1,22 @@
+//elemento que realiza uma ação
+
+//first class citizens porque trabalha como qualquer outro tipo de dado
+
+function somar (a, b) {
+    return a + b;
+}
+
+const execute = somar;
+
+console.log(execute(1, 2));
+
+//Higher-Order Function
+
+//quando função não retorna nada retorna undefined
+//com return tem retorno explicito
+
+let valores = [1, 3.4, true, somar]
+
+for(let i = 0; i < valores.length; i++) {
+    console.log(typeof(valores[i]));
+}
---
+++ js-basics/notes/var_let_const.js
@@ -0,0 +1,21 @@
+//Sempre declare variáveis usando let, é a forma mais moderna. Evita erros no programa e melhora o código, mantém o escopo local.
+
+//var tem escopo global!
+// O escopo é definido a partir das chaves {}
+
+//Corre o risco também de redeclarar variáveis com o mesmo nome.
+
+// const como o nome diz é constante, não pode ser alterada.
+
+//Constante vs Mutação
+
+const curso = {nome: "JavaScript"};
+console.log(curso.nome);
+
+//Podemos realizar mutações em dados contidos em uma constante.
+
+curso.nome = "JavaScript Avançado";
+console.log(curso.nome);
+
+// A partir da constante estamos mudando o valor do atributo nome
+
